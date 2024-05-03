bff54221deeb2e3cff485d7a277903202f4f7609
Lexo783 <40041389+Lexo783@users.noreply.github.com>
2024-05-03 11:38:37 +0200

    add shader

--- cmake-build-debug-visual-studio/CMakeFiles/VerifyGlobs.cmake
+++ cmake-build-debug-visual-studio/CMakeFiles/VerifyGlobs.cmake
@@ -28,6 +28,8 @@ endif()
 file(GLOB_RECURSE NEW_GLOB LIST_DIRECTORIES false "C:/Users/flore/OneDrive/Documents/Unreal Projects/MyFuckingEngine/res/*")
 set(OLD_GLOB
   "C:/Users/flore/OneDrive/Documents/Unreal Projects/MyFuckingEngine/res/.gitkeep"
+  "C:/Users/flore/OneDrive/Documents/Unreal Projects/MyFuckingEngine/res/fragment.glsl"
+  "C:/Users/flore/OneDrive/Documents/Unreal Projects/MyFuckingEngine/res/vertex.glsl"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
