49c35b3e8839d61dae79454fc61fc97b855999cd
Lexo783 <40041389+Lexo783@users.noreply.github.com>
2024-05-03 12:44:17 +0200

    shader

--- cmake-build-debug-visual-studio/res/vertex.glsl
+++ cmake-build-debug-visual-studio/res/vertex.glsl
@@ -4,5 +4,6 @@ layout(location = 0) in vec2 in_position;
 
 void main()
 {
-    gl_Position = vec4(in_position, 0., 1.);
+    vec2 position = in_position + vec2(.5, .5);
+    gl_Position = vec4(position, 0., 1.);
 }
