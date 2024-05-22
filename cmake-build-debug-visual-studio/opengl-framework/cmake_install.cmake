8496ca5eaf8de95d56bfc947513abed5daa98a7b
Lexo783 <40041389+Lexo783@users.noreply.github.com>
2024-05-22 10:24:34 +0200

    add framework

--- cmake-build-debug-visual-studio/opengl-framework/cmake_install.cmake
+++ cmake-build-debug-visual-studio/opengl-framework/cmake_install.cmake
@@ -39,6 +39,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("C:/Users/flore/OneDrive/Documents/Unreal Projects/MyFuckingEngine/cmake-build-debug-visual-studio/opengl-framework/lib/img/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("C:/Users/flore/OneDrive/Documents/Unreal Projects/MyFuckingEngine/cmake-build-debug-visual-studio/opengl-framework/lib/exe_path/cmake_install.cmake")
 endif()
 
