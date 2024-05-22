d37096d41f516c0170e432346816e920b359d37a
Lexo783 <40041389+Lexo783@users.noreply.github.com>
2024-05-22 14:54:07 +0200

    add camera

--- cmake-build-debug-visual-studio/res/vertex.glsl
+++ cmake-build-debug-visual-studio/res/vertex.glsl
@@ -1,12 +1,24 @@
 #version 410
 
 layout(location = 0) in vec2 in_position;
+uniform mat4 view_projection_matrix;
+
+
+void main()
+{
+    gl_Position = view_projection_matrix * vec4(in_position, 0., 1.);
+}
+
+
+/**
+2D
+void main()
+{
+
 uniform vec2 moveCube; // Vous pouvez mettre le type que vous voulez, et le nom que vous voulez
 uniform float aspect_ratio = 1;
 uniform bool fullScreen = true;
 
-void main()
-{
     vec2 position = in_position;
     if(!fullScreen){
         position += moveCube;
@@ -16,3 +28,4 @@ void main()
     gl_Position = vec4(position, 0., 1.);
 
 }
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,57 @@
 #include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
+#include "glm/ext/matrix_clip_space.hpp"
+
+int main()
+{
+    // Initialisation
+    gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
+    gl::maximize_window(); // On peut la maximiser si on veut
+
+    auto camera = gl::Camera{};
+    gl::set_events_callbacks({camera.events_callbacks()});
+
+    glm::mat4 const projection_matrix = glm::infinitePerspective(1.f /*field of view in radians*/, gl::framebuffer_aspect_ratio() /*aspect ratio*/, 0.001f /*near plane*/);
+
+    auto const rectangle_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0}},
+            .data   = {
+                    -0.5f, -0.5f, // Position2D du 1er sommet
+                    +0.5f, -0.5f, // Position2D du 2ème sommet
+                    +0.5f, +0.5f, // Position2D du 3ème sommet
+                    -0.5f, +0.5f  // Position2D du 4ème sommet
+                    },
+                    }},
+                    .index_buffer   = {
+                0, 1, 2, // Indices du premier triangle : on utilise le 1er, 2ème et 3ème sommet
+                0, 2, 3  // Indices du deuxième triangle : on utilise le 1er, 3ème et 4ème sommet
+                },
+                }};
+
+
+    auto const shader = gl::Shader{{
+        .vertex   = gl::ShaderSource::File{"res/vertex.glsl"},
+        .fragment = gl::ShaderSource::File{"res/fragment.glsl"},
+        }};
+
+    while (gl::window_is_open())
+    {
+        glClearColor(0.f, 0.f, 1.f, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
+        glClear(GL_COLOR_BUFFER_BIT); // Exécute concrètement l'action d'appliquer sur tout l'écran la couleur choisie au-dessus
+
+        shader.bind();
+
+        // on peut récupérer la matrice de vue avec :
+        glm::mat4 const view_matrix = camera.view_matrix();
+        shader.set_uniform("view_projection_matrix", projection_matrix * view_matrix);
+        shader.set_uniform("color", glm::vec4{.1, 0.5f, .5f, 1.});
+        rectangle_mesh.draw();
+    }
+}
+
+/***
+ * Algo pour du 2D
+ * @return Void main exécute le shader
 
 int main()
 {
@@ -64,3 +117,4 @@ int main()
         rectangle_mesh.draw(); // C'est ce qu'on appelle un "draw call" : on envoie l'instruction à la carte graphique de dessiner notre mesh.
     }
 }
+*/
